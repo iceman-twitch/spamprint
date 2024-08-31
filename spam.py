c6823d8519388c0588c394e091151095ba018011
ICEMAN <iceman.twitch.contact@gmail.com>
2024-09-01 00:12:22 +0200

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,5 +3,5 @@ text = "Ha kell koveto kovesd be es irj ra ig: IGNAME"
 bonus_text = " {}x"
 
 
-for i in range(100):  # loop 5 times
+for i in range(100):  # loop 100 times
     print( text + bonus_text.format(i+1) )  # add bonus text with loop number and 'x'
